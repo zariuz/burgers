6db89402f1d5e639bf299bb847c730801fdb09bb
zariuz <nechetov@hotmail.com>
2017-08-20 18:42:05 +0300

    add yandex map

--- js/main.js
+++ js/main.js
@@ -1,16 +1,59 @@
+// Яндекс карта!
+
+ymaps.ready(init);
+var myMap;
+
+function init() {
+    myMap = new ymaps.Map("map", {
+        center: [59.924,30.385],
+        zoom: 11,
+        controls: []
+    });
+
+    myPlacemark = new ymaps.Placemark([59.973,30.311], {
+        hintContent: 'CПБ!',
+        balloonContent: 'Вкусняшки тут! :)'
+    }, {
+        iconLayout: 'default#image',
+        iconImageHref: './img/content/map-marker.png',
+        iconImageSize: [46, 57],
+        iconImageOffset: [-22, -57]
+    });
+    myPlacemark2 = new ymaps.Placemark([59.944,30.380], {
+        hintContent: 'CПБ!',
+        balloonContent: 'Вкусняшки тут! :)'
+    }, {
+        iconLayout: 'default#image',
+        iconImageHref: './img/content/map-marker.png',
+        iconImageSize: [46, 57],
+        iconImageOffset: [-22, -57]
+    });
+    myPlacemark3 = new ymaps.Placemark([59.914, 30.493], {
+        hintContent: 'CПБ!',
+        balloonContent: 'Вкусняшки тут! :)'
+    }, {
+        iconLayout: 'default#image',
+        iconImageHref: './img/content/map-marker.png',
+        iconImageSize: [46, 57],
+        iconImageOffset: [-22, -57]
+    });
+    myPlacemark4 = new ymaps.Placemark([59.886,30.314], {
+        hintContent: 'CПБ!',
+        balloonContent: 'Вкусняшки тут! :)'
+    }, {
+        iconLayout: 'default#image',
+        iconImageHref: './img/content/map-marker.png',
+        iconImageSize: [46, 57],
+        iconImageOffset: [-22, -57]
+    });
+
+    myMap.behaviors.disable('scrollZoom');
+    myMap.geoObjects.add(myPlacemark).add(myPlacemark2).add(myPlacemark3).add(myPlacemark4);
+}
+
 // Меню первой секции!
 
-//$(document).ready(function() {
-    
-//     $('.animate').on('click', function(e){
-//         e.preventDefault()
-//          $('.block').slideToggle(3000);
-//        })
-
-// }
-
 var menu = document.getElementsByClassName('first-header__navigation')[0];
-
 var menuAccord = document.getElementsByClassName('first-header__hamburger-link')[0];
 menuAccord.onclick = function() {
     var active = menu.classList.value;
@@ -21,3 +64,4 @@ menuAccord.onclick = function() {
         menu.classList.remove('first-header__navigation-activ');
     }
 }
+

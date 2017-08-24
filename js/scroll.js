82895d54dbe8ff74f56fcd88ec91f51e1078c738
zariuz <nechetov@hotmail.com>
2017-08-24 22:19:36 +0300

    one page scroll fixed 2

--- js/scroll.js
+++ js/scroll.js
@@ -26,23 +26,57 @@ $(function() {
 
     }
 
+    var defineSections = function (sections) {
+        var activeSection = sections.filter('.active');
+        return {
+            activeSection: activeSection,
+            nextSection: activeSection.next(),
+            prevSection: activeSection.prev()
+        }
+    }
+
     $('.wrapper').on('wheel', function (e) {
 
         var deltaY = e.originalEvent.deltaY;
-        var activeSection = sections.filter('.active');
-        var nextSection = activeSection.next();
-        var prevSection = activeSection.prev();
+        var section = defineSections(sections);
 
-        if (deltaY > 0 && nextSection.length) {
+
+        if (deltaY > 0 && section.nextSection.length) {
             console.log('вверх');
-            performTransition(nextSection.index());
+            performTransition(section.nextSection.index());
         }
 
-        if (deltaY < 0 && prevSection.length) {
+        if (deltaY < 0 && section.prevSection.length) {
             console.log('вниз');
-            performTransition(prevSection.index());
+            performTransition(section.prevSection.index());
         }
 
     })
+
+    $(document).on('keydown', function (e) {
+        var section = defineSections(sections);
+    
+        switch (e.keyCode) {
+            case 40: // up
+                if (section.nextSection.length) {
+                    performTransition(section.nextSection.index());
+                }
+                break;
+            case 38: // down
+                if (section.prevSection.length) {
+                    performTransition(section.prevSection.index());
+                }
+                break;
+        }
+    });
+
+    $('[data-scroll-to]').on('click touchstart', function (e) {
+        e.preventDefault();
+    
+        var elem = $(e.target);
+        var sectionNum = parseInt(elem.attr('data-scroll-to'));
+        performTransition(sectionNum);
+    });
+    
 
 });
